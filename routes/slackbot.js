80afea77de47a6edc908da993425ec72cfe57a11
Ralph Landon <ralph7c2@gmail.com>
2018-09-10 16:50:33 -0700

    Add /slashbirthday

--- routes/slackbot.js
+++ routes/slackbot.js
@@ -2,26 +2,32 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
-/* GET users listing. */
-router.post("/birthdayService", function(req, res, next) {
+router.post("/slashbirthday", async function(req, res) {
+  console.log(req.body);
+});
+
+router.post("/birthdayService", async function(req, res, next) {
   console.log(req.body);
   let payload = req.body;
-
-  res.sendStatus(200);
 
   if (payload.type === "url_verification" && payload.challenge) {
     return res.json({ challenge: payload.challenge });
   } else if (payload.event.type === "member_joined_channel") {
-    axios.post("https://slack.com/api/chat.postMessage", {
+    let result = await axios.post("https://slack.com/api/chat.postMessage", {
       text: "Welcome <@" + payload.event.user + ">!",
       channel: payload.event.channel
     });
+    console.log(result);
   } else if (payload.event.type == "message") {
-    axios.post("https://slack.com/api/chat.postMessage", {
-      text: "Message from <@" + paload.event.user + ">",
+    console.log("Message event");
+    let result = await axios.post("https://slack.com/api/chat.postMessage", {
+      text: "Message from <@" + payload.event.user + ">",
       channel: payload.event.channel
     });
+    console.log(result);
   }
+
+  res.sendStatus(200);
 });
 
 module.exports = router;
