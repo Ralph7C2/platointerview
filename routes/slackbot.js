ea055ebf8aa55f503319aaa809dd02bc201d2573
Ralph Landon <ralph7c2@gmail.com>
2018-09-10 16:25:59 -0700

    Added some events

--- routes/slackbot.js
+++ routes/slackbot.js
@@ -1,11 +1,26 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const axios = require8("axios");
 
 /* GET users listing. */
 router.post("/birthdayService", function(req, res, next) {
   console.log(req.body);
-  if (req.body.type === "url_verification" && req.body.challenge) {
-    return res.json({ challenge: req.body.challenge });
+  let payload = req.body;
+
+  res.sendStatus(200);
+
+  if (payload.type === "url_verification" && payload.challenge) {
+    return res.json({ challenge: payload.challenge });
+  } else if (payload.event.type === "member_joined_channel") {
+    axios.post("https://slack.com/api/chat.postMessage", {
+      text: "Welcome <@" + payload.event.user + ">!",
+      channel: payload.event.channel
+    });
+  } else if (payload.event.type == "message") {
+    axios.post("https://slack.com/api/chat.postMessage", {
+      text: "Message from <@" + paload.event.user + ">",
+      channel: payload.event.channel
+    });
   }
   res.json({});
 });
