10278af56e3bc74d83ab0d8bf1fe28691830f786
Ralph Landon <ralph7c2@gmail.com>
2018-09-10 16:31:10 -0700

    Fixed error

--- routes/slackbot.js
+++ routes/slackbot.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const axios = require8("axios");
+const axios = require("axios");
 
 /* GET users listing. */
 router.post("/birthdayService", function(req, res, next) {
