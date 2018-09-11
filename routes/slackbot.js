d17751cb1885b655752f9169d27e30e677903c57
Ralph Landon <ralph7c2@gmail.com>
2018-09-10 17:31:30 -0700

    No Json to slack api??

--- routes/slackbot.js
+++ routes/slackbot.js
@@ -18,19 +18,21 @@ router.post("/birthdayService", async function(req, res, next) {
   if (payload.type === "url_verification" && payload.challenge) {
     return res.json({ challenge: payload.challenge });
   } else if (payload.event.type === "member_joined_channel") {
-    let result = await axios.post("https://slack.com/api/chat.postMessage", {
-      token: "xoxb-432998031813-432733915699-z7s6ZFP1HNZq1NvkiHi4N5eT",
-      text: "Welcome <@" + payload.event.user + ">!",
-      channel: payload.event.channel
-    });
+    let result = await axios.post(
+      "https://slack.com/api/chat.postMessage?token=xoxb-432998031813-432733915699-z7s6ZFP1HNZq1NvkiHi4N5eT&" +
+        "text='Welcome'&" +
+        "channel=" +
+        payload.event.channel
+    );
     console.log(result.data);
   } else if (payload.event.type == "message") {
     console.log("Message event");
-    let result = await axios.post("https://slack.com/api/chat.postMessage", {
-      token: "xoxb-432998031813-432733915699-z7s6ZFP1HNZq1NvkiHi4N5eT",
-      text: "Message from <@" + payload.event.user + ">",
-      channel: payload.event.channel
-    });
+    let result = await axios.post(
+      "https://slack.com/api/chat.postMessage?token=xoxb-432998031813-432733915699-z7s6ZFP1HNZq1NvkiHi4N5eT&" +
+        "text='Welcome'&" +
+        "channel=" +
+        payload.event.channel
+    );
     console.log(result.data);
   }
 
